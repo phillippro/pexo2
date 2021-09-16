1334898d8be0845cd847897049aa3b8f87eb436b
Fabo Feng <fengfabo@gmail.com>
2021-09-16 22:53:24 +0800

    0916

--- code/test.py
+++
@@ -1,3 +0,0 @@
-import requests
-r = requests.post('https://gea.esac.esa.int/tap-server/tap/sync', headers={'Content-Type':'application/x-www-form-urlencoded'}, data={'request':'getCapabilities'})
-print (r.text)
--- code/test_proctime.R
+++
@@ -1,9 +0,0 @@
-t0 <- proc.time()
-Ntry <- 100
-star <- 'HD128620'
-type <- 'rv'
-ParNew <- Par
-for(i in 1:Ntry){
-     fit <- time_Ta2te(OutObs[[star]][[type]],ParNew,fit=TRUE,OutTime0=OutTime0[[star]][[type]])
-}
-t1 <- fit_TimeCount(t0,'dur1',ofac=Ntry)
